7efd84dc4ea7a9da06f181a8982edbdc73d8d282
Javul Mulla <javulmulla8@gmail.com>
2022-10-18 12:28:44 +0530

    Added:Determine logging levels,logging pattern based on this application.

--- src/main/java/com/bridgelabz/employpayrollapp/EmployPayrollAppApplication.java
+++ src/main/java/com/bridgelabz/employpayrollapp/EmployPayrollAppApplication.java
@@ -3,13 +3,14 @@ package com.bridgelabz.employpayrollapp;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @Slf4j
 public class EmployPayrollAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EmployPayrollAppApplication.class, args);
-        log.info("Employee payroll app started");
+        ApplicationContext context = SpringApplication.run(EmployPayrollAppApplication.class, args);
+        log.info("Employee payroll app started {} Environment", context.getEnvironment().getProperty("environment"));
     }
 }
