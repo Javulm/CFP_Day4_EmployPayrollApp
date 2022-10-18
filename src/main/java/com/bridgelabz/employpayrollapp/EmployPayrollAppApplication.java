c673f937fdd0868ca7fdc885d8dca76d9b0d425c
Javul Mulla <javulmulla8@gmail.com>
2022-10-18 09:48:43 +0530

    Initial commit.

---
+++ src/main/java/com/bridgelabz/employpayrollapp/EmployPayrollAppApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employpayrollapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployPayrollAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployPayrollAppApplication.class, args);
+	}
+
+}
