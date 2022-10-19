1d6f3af77dee58c85f2e5da860310066ae7a3b5e
Javul Mulla <javulmulla8@gmail.com>
2022-10-19 12:17:16 +0530

    Added: validation on employee payroll DTO.

--- src/main/java/com/bridgelabz/employpayrollapp/dto/EmployeePayrollDto.java
+++ src/main/java/com/bridgelabz/employpayrollapp/dto/EmployeePayrollDto.java
@@ -1,13 +1,12 @@
 package com.bridgelabz.employpayrollapp.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.*;
+import java.time.LocalDate;
 import java.util.List;
 
 @Data
@@ -18,13 +17,27 @@ public class EmployeePayrollDto {
     @Pattern(regexp = "^[A-Z][a-zA-Z\\s]{2,}$", message = "Employee name Invalid!")
     @NotEmpty(message = "Employee name cannot be null.")
     private String name;
+
+    @NotNull(message = "salary Should not be Empty")
     @Max(value = 1000000, message = "maximum wage should not exceed than 1000000")
     @Min(value = 500, message = "minimum wage should be more than 500.")
     private long salary;
+
+    @Pattern(regexp = "male|female", message = "Gender Needs to be Male or Female")
     private String gender;
-    private String startDate;
+
+    @JsonFormat(pattern = "dd MMM yyyy")
+    @NotNull(message = "startDate Should not be Empty")
+    @PastOrPresent(message = "startDate Should not be past or present Date")
+    private LocalDate startDate;
+
+    @NotBlank(message = "Note cannot be empty")
     private String note;
+
+    @NotBlank(message = "Profile Pic should not be Empty")
     private String profilePic;
+
+    @NotNull(message = "department should not be Empty")
     private List<String> departments;
 
 }
